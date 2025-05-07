2d33207f14f8759c9f53bb3afed1c47fcaaabe87
Dmitri Priimak <priimak@gmail.com>
2025-05-07 09:11:02 -0500

    Set explicit style for menu items

--- src/hspro/gui/menus/menu_bar.py
+++ src/hspro/gui/menus/menu_bar.py
@@ -11,6 +11,22 @@ class MainMenuBar(QMenuBar):
         super().__init__(None)
 
         self.file_menu = FileMenu(self, app)
+        self.trace_menu = TraceMenu(self, app)
+        self.help_menu = HelpMenu(self)
+
         self.addMenu(self.file_menu)
-        self.addMenu(TraceMenu(self, app))
-        self.addMenu(HelpMenu(self))
+        self.addMenu(self.trace_menu)
+        self.addMenu(self.help_menu)
+
+        self.setStyleSheet(
+            """
+            QMenu {
+                background-color: #ebeff4;
+            }
+            
+            QMenu::item:selected {
+                background-color: #3daee9;
+                color: white;
+            }
+            """
+        )
